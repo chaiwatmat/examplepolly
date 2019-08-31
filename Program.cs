cd61bb62c2c6483de7a1b98e4f96fa8b52e304a2
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-08-31 22:57:38 +0700

    polly with retry works

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Polly;
 
 namespace console {
     class Program {
@@ -8,8 +9,17 @@ namespace console {
             //create the http client
             var httpClient = new HttpClient ();
 
-            //call the httpbin service
-            var response = await httpClient.PostAsync ("https://httpbin.org/status/200,408", null);
+            //call the httpbin service with Polly
+            var response = await Policy
+                .HandleResult<HttpResponseMessage> (message => !message.IsSuccessStatusCode)
+                .WaitAndRetryAsync (new [] {
+                    TimeSpan.FromSeconds (1),
+                        TimeSpan.FromSeconds (3),
+                        TimeSpan.FromSeconds (9)
+                }, (result, timeSpan, retryCount, context) => {
+                    Console.WriteLine ($"Request failed with {result.Result.StatusCode}. Retry count = {retryCount}. Waiting {timeSpan} before next retry. ");
+                })
+                .ExecuteAsync (() => httpClient.PostAsync ("https://httpbin.org/status/200,401,500", null));
 
             if (response.IsSuccessStatusCode)
                 Console.WriteLine ("Response was successful with 200");
